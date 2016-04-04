b35324fbe2ae21494de0f1f1ca3adc7db8a7e5e2
Martti Naaber <martnaa@greeny.cs.tlu.ee>
2016-04-04 12:23:49 +0300

    added stuff

---
+++ getFeed.php
@@ -0,0 +1,6 @@
+<?php
+	//getFeed.php?search=%23Tallinn
+	//https://github.com/J7mbo/twitter-api-php/blob/master/TwitterAPIExchange.php
+	require_once("twitterApiExhange.php");
+	require_once("config.php");
+?>
